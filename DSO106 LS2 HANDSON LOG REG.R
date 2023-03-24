f3debad9071c5ca11e8cb9418b96547c9e65a0ae
Dorothy Couch <97361877+DodiCouch@users.noreply.github.com>
2023-03-24 14:32:16 -0800

    Update DSO106 LS2 HANDSON LOG REG.R

--- DSO106 LS2 HANDSON LOG REG.R
+++ DSO106 LS2 HANDSON LOG REG.R
@@ -9,19 +9,6 @@ library("lmtest")
 library("popbio")
 library("e1071")
 
-##Geologists have long known that the presence of antimony in a ground 
-#sample can be a good indication that there is a gold deposit nearby. 
-#In the attached data, you will find antimony (Sb) levels for 64 different locations, 
-#and whether or not gold was found nearby. 
-#The "gold" column is coded as 0 for no gold present, and 1 for gold present.
-
-#Use logistic regression in R to create a prediction model 
-#that will give the probability of the presence of gold as a response. 
-#Your complete report should contain the following information:
-  
-#Testing and correction for assumptions if necessary.
-#Interpretation of the results in layman's terms.
-
 #### Read in data ###
 
 read.csv('/Users/dorothycouch/Documents/minerals.csv')
@@ -29,8 +16,8 @@ read.csv('/Users/dorothycouch/Documents/minerals.csv')
 # Antimony level = IV 
 # Gold presence yes/no = DV
 
-#Hypothesis is that higher antimony levels mean higher possibility of gold being present nearby
-# Assess probability of the gold being present using these data.
+''''Hypothesis is that higher antimony levels mean higher possibility of gold being present nearby
+Assess probability of the gold being present using these data. '''
 
 #data is already coded for 0/1.
 
